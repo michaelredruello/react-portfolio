6c0896c490e6f0203cc09114e708e6d296beaa1d
Michael Redruello <redruellomichael@gmail.com>
2022-12-03 20:39:16 +0100

    Logo
    Logo setup and style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
@@ -25,9 +26,13 @@ const Home = () => {
   ]
 
   useEffect(() => {
-    setTimeout(() => {
+    let timeoutId = setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 4000)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
   }, [])
 
   return (
@@ -39,7 +44,10 @@ const Home = () => {
           <br />
           <span className={`${letterClass} _13`}>I</span>
           <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
+          <img
+            src={LogoTitle}
+            alt="JavaScript Developer Name, Web Developer Name"
+          />
           <AnimatedLetters
             letterClass={letterClass}
             strArray={nameArray}
@@ -52,11 +60,12 @@ const Home = () => {
             idx={22}
           />
         </h1>
-        <h2>Frontend Developer / Javascript Expert / Youtuber</h2>
+        <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
         <Link to="/contact" className="flat-button">
           CONTACT ME
         </Link>
       </div>
+      <Logo />
     </div>
   )
 }
